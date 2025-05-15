f9713ff483037c5627cead233a93d49f605ffe1d
saiful-47 <saifulcse47@gmail.com>
2025-05-16 00:06:41 +0100

    undefined route handler add

--- my-api/app.js
+++ my-api/app.js
@@ -4,4 +4,11 @@ const app = new express();
 
 app.use('/api/v1', router);
 
+
+// Catch-all for undefined routes (404 handler)
+app.use((req, res, next) => {
+    res.status(404).send('404 Not Found');
+});
+
 module.exports = app;
+
